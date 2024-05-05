702c7657b42c197a6049d469e1e88c078d8c2a91
Faiaz Hossain <faiazhossain10@gmail.com>
2024-05-05 09:17:12 +0600

    feat: meal page added

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,37 @@
 import Link from "next/link";
-import React from "react";
+import classes from "./page.module.css";
+import React, { Suspense } from "react";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-const Meals = () => {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
-    <main>
-      <h1>Meals page</h1>
-      <Link href="/meals/something">Something</Link>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
-};
-
-export default Meals;
+}
