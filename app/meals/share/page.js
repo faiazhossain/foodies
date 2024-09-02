8f92144fc67830f033c3dfede972425f7a3e22e6
Faiaz Hossain <faiazhossain10@gmail.com>
2024-09-02 18:05:19 +0600

    feat: ux for share meal

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,7 @@
-"use client"; 
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
+import { MealsFormSubmit } from "@/components/meals/meals-form-submit";
 export default function ShareMealPage() {
   return (
     <>
@@ -42,7 +42,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit></MealsFormSubmit>
           </p>
         </form>
       </main>
